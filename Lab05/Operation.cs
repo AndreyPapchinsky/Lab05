9fec3d976f63ea3dc278d976f57295bba493b986
AndreyPapchinsky <Ha.xJle6ywek@yandex.ru>
2021-11-27 21:18:31 +0300

    Добавьте файлы проекта.

---
+++ Lab05/Operation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab05
+{
+    class Operation
+    {
+        public static double STriangle(double a, double b, double c, out double s)
+        {
+            if (Operation.CheckTriangle(a, b, c))
+            {
+                double p2 = a + b + c;
+                double p = p2 / 2;
+                s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return s;
+            }
+            else
+            {
+                s = 0;
+                return s;
+            }
+        }
+        private static bool CheckTriangle(double a, double b, double c)
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                if (a + b <= c || a + c <= b || b + c <= a)
+                    return false;
+                else
+                    return true;
+            }
+            else
+                return false;
+        }
+        public static double STriangle(double a, out double s)
+        {
+            double p2 = a * 3;
+            double p = p2 / 2;
+            s = Math.Sqrt(p * (p - a) * (p - a) * (p - a));
+            return s;
+        }
+    }
+}
---
+++ Lab05/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Lab05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int type;
+            double a, b, c, s;
+
+            Console.WriteLine(" Расчет площади треугольника с помощью метода.");
+            Console.WriteLine(" Выберите тип треугольника: \n" +
+                " 1 - Разносторонний  \n" +
+                " 2 - Равносторонний  \n" +
+                " 3 - Равнобедренный  \n" +
+                " 4 - Остроугольный   \n" +
+                " 5 - Прямоугольный   \n" +
+                " 6 - Тупоугольный");
+            type = int.Parse(Console.ReadLine());
+
+            switch (type)
+            {
+                case 2:
+                    Console.WriteLine("Ведите значение стороны треуголника: ");
+                    a = double.Parse(Console.ReadLine());
+                    s = Operation.STriangle(a, out s);
+                    Console.WriteLine("Площадь треугольника: " + s);
+                    break;
+
+                default:
+                    Console.WriteLine("Ведите значение стороны треуголника (a): ");
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ведите значение стороны треуголника (b): ");
+                    b = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Ведите значение стороны треуголника (c): ");
+                    c = double.Parse(Console.ReadLine());
+
+                    s = Operation.STriangle(a, b, c, out s);
+
+                    if (s>0)
+                    {
+                        Console.WriteLine("Площадь треугольника: " + s);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Недопустимые значения сторон!");
+                    }
+
+                    break;
+            }
+        }
+    }
+}
